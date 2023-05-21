9bbbaa55a2d87a197990d9b1058c8db659cb0383
senlin1588 <lcs201010@163.com>
2023-05-21 19:59:21 +0800

    修改inde.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function(param1,param2,param3){
-	let param4 = "param4 = " param1+param2+param3;
+function add(param1,param2,param3){
+	let param4 = "param4 = " + param1+param2+param3;
 	console.log(param1,param2,param3);
 	console.log(param4);
+	return {
+		"param1":param1,
+		"param2":param2,
+		"param3":param3,
+		"param4":param4,
+	}
 }
+module.exports = add;
